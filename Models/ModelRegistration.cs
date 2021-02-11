5924eea82ea17c45ae4c27b7ce18264c5a8ba3a4
Aditya Patel <55842340+adityapatel9619@users.noreply.github.com>
2021-02-11 17:11:25 +0530

    new

--- Models/ModelRegistration.cs
+++ Models/ModelRegistration.cs
@@ -1,11 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Creating_MVC_Form.Models
+namespace MVC_Form_Intern_Aditya.Models
 {
     public class ModelRegistration
     {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Addressdetails { get; set; }
+        public string Age { get; set; }
+        public string Mobilenumber { get; set; }
+        public string Emailid { get; set; }
+        public string Qualification { get; set; }
+        public string Userid { get; set; }
     }
 }
