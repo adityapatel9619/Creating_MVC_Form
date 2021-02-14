ccea8a4276439bcbbde80fa833791769e09cd198
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2021-02-14 23:09:19 +0530

    aditya

---
+++ API/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Creating_MVC_Form.Models;
+
+namespace Creating_MVC_Form.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = _service.AddInventoryItems(items);
+
+            if (inventoryItems == null)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+    }
+}
--- API/RegistrationController.cs
+++ API/RegistrationController.cs
@@ -1,69 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MVC_Form_Intern_Aditya.DB;
-using Microsoft.Extensions.Options;
-using MVC_Form_Intern_Aditya.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Creating_MVC_Form.DB;
+//using Microsoft.Extensions.Options;
+//using MVC_Form_Intern_Aditya.Models;
 
-namespace MVC_Form_Intern_Aditya.API
-{
-    //[Route("api/[controller]")]
-    //[ApiController]
-    //public class RegistrationController : ControllerBase
-    //{
-    //}
+//namespace Creating_MVC_Form.API
+//{
+//    //[Route("api/[controller]")]
+//    //[ApiController]
+//    //public class RegistrationController : ControllerBase
+//    //{
+//    //}
 
 
-    [Route("api/[controller]/[Action]")]
-    [ApiController]
-    public class RegisterController : ControllerBase
-    {
-        DB.DBRegistration datatest = new DB.DBRegistration();
+//    [Route("api/[controller]/[Action]")]
+//    [ApiController]
+//    public class RegisterController : ControllerBase
+//    {
+//        DB.DBRegistration datatest = new DB.DBRegistration();
 
-        private readonly IOptions<ModelConnectionString> appSettings;
-        public RegisterController(IOptions<ModelConnectionString> app)
-        {
-            appSettings = app;
-        }
+//        private readonly IOptions<ModelConnectionString> appSettings;
+//        public RegisterController(IOptions<ModelConnectionString> app)
+//        {
+//            appSettings = app;
+//        }
 
-        [HttpGet]
-        [ActionName("GetRegisterlist")]
-        // api/Register/GetRegisterlist
-        public IEnumerable<ModelRegistration> GetRegisteredlist()
-        {
-            return datatest.GetRegisteredlist(appSettings.Value.DefaultConnection);
-        }
-        [HttpGet]
-        [ActionName("EditRegisteredlist")]
-        // api/Register/GetRegisterlist?id=1
-        public IEnumerable<ModelRegistration> EditRegistered(int id)
-        {
-            return datatest.EditRegistered(id, appSettings.Value.DefaultConnection);
-        }
-        [HttpPost]
-        [ActionName("SaveRegister")]
-        //  api/Register/SaveRegister
-        public string Post(ModelRegistration stud)
-        {
-            return datatest.Post(stud, appSettings.Value.DefaultConnection);
-        }
-        [HttpPut]
-        [ActionName("UpdateRegister")]
-        //  api/Register/UpdateRegister
-        public string Put(ModelRegistration stud)
-        {
-            return datatest.Put(stud, appSettings.Value.DefaultConnection);
-        }
-        [HttpDelete]
-        [ActionName("DeleteRegister")]
-        //  api/Register/DeleteRegister
-        public string Delete(int id, string deletedBy)
-        {
-            return datatest.Delete(id, deletedBy, appSettings.Value.DefaultConnection);
-        }
-    }
+//        [HttpGet]
+//        [ActionName("GetRegisterlist")]
+//        // api/Register/GetRegisterlist
+//        public IEnumerable<ModelRegistration> GetRegisteredlist()
+//        {
+//            return datatest.GetRegisteredlist(appSettings.Value.DefaultConnection);
+//        }
+//        [HttpGet]
+//        [ActionName("EditRegisteredlist")]
+//        // api/Register/GetRegisterlist?id=1
+//        public IEnumerable<ModelRegistration> EditRegistered(int id)
+//        {
+//            return datatest.EditRegistered(id, appSettings.Value.DefaultConnection);
+//        }
+//        [HttpPost]
+//        [ActionName("SaveRegister")]
+//        //  api/Register/SaveRegister
+//        public string Post(ModelRegistration stud)
+//        {
+//            return datatest.Post(stud, appSettings.Value.DefaultConnection);
+//        }
+//        [HttpPut]
+//        [ActionName("UpdateRegister")]
+//        //  api/Register/UpdateRegister
+//        public string Put(ModelRegistration stud)
+//        {
+//            return datatest.Put(stud, appSettings.Value.DefaultConnection);
+//        }
+//        [HttpDelete]
+//        [ActionName("DeleteRegister")]
+//        //  api/Register/DeleteRegister
+//        public string Delete(int id, string deletedBy)
+//        {
+//            return datatest.Delete(id, deletedBy, appSettings.Value.DefaultConnection);
+//        }
+//    }
 
-}
+//}
